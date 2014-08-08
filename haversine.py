e2d43c88646451acff0e5b181c62b2c5b6195afe
scivision <scivision@users.noreply.github.com>
2014-08-08 11:25:26 -0400

    added angular distance

--- haversine.py
+++ haversine.py
@@ -1,5 +1,12 @@
-from numpy import cos
+from numpy import cos,arcsin,sqrt
 # Michael Hirsch
-# based on: http://en.wikipedia.org/wiki/Haversine
+# from "Astronomical Algorithms" by Jean Meeus Ch. 16 p. 111 (16.5)
+
+
+def angledist(r0,d0,r1,d1):
+    dist_rad = 2.*arcsin( sqrt( haversine(d1-d0) + cos(d0)*cos(d1) * haversine(r1-r0) ) )
+    return dist_rad
+
 def haversine(theta):
+# http://en.wikipedia.org/wiki/Haversine and Meeus p. 111
     return (1-cos(theta)) / 2.
