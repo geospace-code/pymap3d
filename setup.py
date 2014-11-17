3f4445dd3fabbb7e67c40413d045f4c26315f773
Michael Hirsch <scienceopen@users.noreply.github.com>
2014-11-16 20:16:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@
 from distutils.core import setup
 setup(name='coordconv3d',
       version='1.0',
-      py_modules=['azel2radec','haversine','vreckon','coordconv3d'],
+      py_modules=['azel2radec','haversine','vreckon','coordconv3d'],)
       
