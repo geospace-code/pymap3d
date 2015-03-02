8ed3bec12cada3b99e236f64555e98dd6563c7e5
scienceopen <scienceopen@users.noreply.github.com>
2015-03-02 03:23:37 -0500

    upd

--- azel2radec.py
+++ azel2radec.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python3
 from __future__ import division
-from numpy import sin, cos, degrees, radians,arcsin, arctan2, atleast_1d, nan
+from numpy import sin, cos, degrees, radians,arcsin, arctan2, atleast_1d, nan# empty_like
 import sys
-sys.path.append('../astrometry') # git clone https://github.com/scienceopen/astrometry/
-from datetime2hourangle import datetime2sidereal
 
-def azel2radec(az_deg,el_deg,lat_deg,lon_deg,dtime):
+usevallado = True
+if usevallado:
+    sys.path.append('../astrometry') # git clone https://github.com/scienceopen/astrometry/
+    from datetime2hourangle import datetime2sidereal
+#else: #astropy
+#    from astropy import units as u
+#    from astropy.time import Time
+#    from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
+"""
+Astropy 1.0 upgrade not completed yet, so we use Vallado method
+Michael Hirsch
+GPLv3+
+"""
+    
+def azel2radec(az_deg, el_deg, lat_deg, lon_deg, dtime):
 
     """ from D.Vallado Fundamentals of Astrodynamics and Applications p.258-259 """
-    az = atleast_1d(radians(az_deg))
-    el = atleast_1d(radians(el_deg))
-    lat = atleast_1d(radians(lat_deg))
-    lon = atleast_1d(radians(lon_deg))
+    az_deg = atleast_1d(az_deg)
+    el_deg = atleast_1d(el_deg)
+    lat_deg = atleast_1d(lat_deg)
+    lon_deg = atleast_1d(lon_deg)
 
-    if az.shape != el.shape: exit('az and el must be same shape')
-    if lat.shape != lat.shape: exit('lat and lon must be same shape')
+    if az_deg.shape != el_deg.shape: exit('az and el must be same shape')
+    if lat_deg.shape != lon_deg.shape: exit('lat and lon must be same shape')
 
-    #Vallado "algorithm 28" p 268
+    if usevallado:
+        ra_deg, dec_deg = azel2radecvallado(az_deg,el_deg,lat_deg,lon_deg,dtime)
+#    else: #use astropy v1.0 + 
+#        ra_deg = empty_like(az_deg); dec_deg = empty_like(az_deg)
+#        for i,(ad,ed) in enumerate(zip(az_deg, el_deg)):
+#            obs = EarthLocation(lat=lat_deg*u.deg, lon=lon_deg*u.deg)
+#            direc = AltAz(location=obs, obstime=Time(dtime), 
+#                          az=az_deg*u.deg, alt=el_deg*u.deg)
+#            #sky = direc.transform_to(SkyCoord())
+           # az_deg[i] = altaz.az.value
+            #el_deg[i] = altaz.alt.value
+            
+        
+    return ra_deg, dec_deg
+        
+def azel2radecvallado(az_deg,el_deg,lat_deg,lon_deg,dtimen):
+    az = radians(az_deg); el = radians(el_deg)
+    lat = radians(lat_deg); lon = radians(lon_deg)
+     #Vallado "algorithm 28" p 268
     dec = arcsin( sin(el) * sin(lat) + cos(el) * cos(lat) * cos(az) )
 
     lha = arctan2( -(sin(az) * cos(el)) / cos(dec),
@@ -45,10 +76,10 @@ if __name__ == "__main__":
         ra,dec = azel2radec(180.1, 80, 65, -148,parse('2014-04-06T08:00:00Z'))
         assert_almost_equal(ra,166.5032081149338)
         assert_almost_equal(dec,55.000011165405752)
-        exit(0)
+    else:
 
-    dtime = parse(a.time)
-    print(dtime)
-
-    ra,dec = azel2radec(a.azimuth,a.elevation,a.lat,a.lon,dtime)
-    print('ra / dec =',(ra,dec) )
+        dtime = parse(a.time)
+        print(dtime)
+    
+        ra,dec = azel2radec(a.azimuth,a.elevation,a.lat,a.lon,dtime)
+        print('ra / dec =',(ra,dec) )
