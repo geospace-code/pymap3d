345ebd3115ceae351cee7bcf2fb2c67c79e701c4
scienceopen <scienceopen@users.noreply.github.com>
2015-03-10 11:54:46 -0400

    upgrade to astropy

--- azel2radec.py
+++ azel2radec.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 from __future__ import division
-from numpy import sin, cos, degrees, radians,arcsin, arctan2, atleast_1d, nan# empty_like
+from numpy import sin, cos, degrees, radians,arcsin, arctan2, atleast_1d, nan
 import sys
 
-usevallado = True
+usevallado = False
 if usevallado:
     sys.path.append('../astrometry') # git clone https://github.com/scienceopen/astrometry/
     from datetime2hourangle import datetime2sidereal
-#else: #astropy
-#    from astropy import units as u
-#    from astropy.time import Time
-#    from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+else: #astropy
+    from astropy import units as u
+    from astropy.time import Time
+    from astropy.coordinates import SkyCoord, EarthLocation, AltAz, ICRS
 
 """
-Astropy 1.0 upgrade not completed yet, so we use Vallado method
 Michael Hirsch
 GPLv3+
 """
@@ -31,15 +30,13 @@ def azel2radec(az_deg, el_deg, lat_deg, lon_deg, dtime):
 
     if usevallado:
         ra_deg, dec_deg = azel2radecvallado(az_deg,el_deg,lat_deg,lon_deg,dtime)
-#    else: #use astropy v1.0 + 
-#        ra_deg = empty_like(az_deg); dec_deg = empty_like(az_deg)
-#        for i,(ad,ed) in enumerate(zip(az_deg, el_deg)):
-#            obs = EarthLocation(lat=lat_deg*u.deg, lon=lon_deg*u.deg)
-#            direc = AltAz(location=obs, obstime=Time(dtime), 
-#                          az=az_deg*u.deg, alt=el_deg*u.deg)
-#            #sky = direc.transform_to(SkyCoord())
-           # az_deg[i] = altaz.az.value
-            #el_deg[i] = altaz.alt.value
+    else: #use astropy v1.0 + 
+        obs = EarthLocation(lat=lat_deg*u.deg, lon=lon_deg*u.deg)
+        direc = AltAz(location=obs, obstime=Time(dtime), 
+                      az=az_deg*u.deg, alt=el_deg*u.deg)
+        sky = SkyCoord(direc.transform_to(ICRS()))
+        ra_deg = sky.ra.deg
+        dec_deg = sky.dec.deg
             
         
     return ra_deg, dec_deg
@@ -72,10 +69,10 @@ if __name__ == "__main__":
     a = p.parse_args()
 
     if a.selftest:
-        from numpy.testing import assert_almost_equal
+        from numpy.testing import assert_allclose
         ra,dec = azel2radec(180.1, 80, 65, -148,parse('2014-04-06T08:00:00Z'))
-        assert_almost_equal(ra,166.5032081149338)
-        assert_almost_equal(dec,55.000011165405752)
+        assert_allclose(ra,166.5032081149338,rtol=1e-2)
+        assert_allclose(dec,55.000011165405752,rtol=1e-2)
     else:
 
         dtime = parse(a.time)
