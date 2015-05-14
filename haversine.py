fa2f4f229acae1095fea051e6ffb289c29c7e155
scivision <scivision@users.noreply.github.com>
2015-05-14 14:18:31 -0400

    cleanup

--- haversine.py
+++ haversine.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 from __future__ import division
-from numpy import cos,arcsin,sqrt,radians,degrees,nan
+from numpy import cos,arcsin,sqrt,radians,degrees
 """
 Michael Hirsch
 from "Astronomical Algorithms" by Jean Meeus Ch. 16 p. 111 (16.5)
 gives angular distance in degrees between two rightAscension,Declination 
-points in the sky
+points in the sky.  Neglecting atmospheric effects, of course.
 
 inputs:
 r0,d0: for first point, rightAscension,Declination [degrees]
 r1,d1: for second point, rightAscension,Declination [degrees]
 
-Note: adding decimal points to the constants made 0 difference in 
-%timeit execution time
+Note: adding decimal points to the constants made 0 difference in %timeit execution time
 GPLv3+ license
 """
 
@@ -28,20 +27,18 @@ def angledist(r0,d0,r1,d1):
     return degrees(dist_rad)
 
 def haversine(theta):
-    """ http://en.wikipedia.org/wiki/Haversine and Meeus p. 111 """
+    """ 
+    http://en.wikipedia.org/wiki/Haversine 
+    Meeus p. 111 """
     return (1-cos(theta)) / 2
 
 if __name__ == '__main__': #pragma: no cover
     from argparse import ArgumentParser
     p = ArgumentParser(description="computes angular distance between two points in sky")
-    p.add_argument('r0',help='right ascension of first point [degrees]',
-                   type=float,nargs='?',default=nan)
-    p.add_argument('d0',help='declination of first point [degrees]',
-                   type=float,nargs='?',default=nan)
-    p.add_argument('r1',help='right ascension of second point [degrees]',
-                   type=float,nargs='?',default=nan)
-    p.add_argument('d1',help='declination of second point [degrees]',
-                   type=float,nargs='?',default=nan)
+    p.add_argument('r0',help='right ascension of first point [degrees]',type=float)
+    p.add_argument('d0',help='declination of first point [degrees]',type=float)
+    p.add_argument('r1',help='right ascension of second point [degrees]',type=float)
+    p.add_argument('d1',help='declination of second point [degrees]', type=float)
     a = p.parse_args()
     
     dist_deg = angledist(a.r0,a.d0,a.r1,a.d1)
