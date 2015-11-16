65b4dd7f570f1f691cc7c24ad20547a11717b454
michael <scienceopen@users.noreply.github.com>
2015-11-16 01:50:18 -0500

    travis

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from setuptools import setup 
+from setuptools import setup
+import subprocess
 
 with open('README.rst','r') as f:
 	long_description = f.read()
@@ -13,4 +14,8 @@ setup(name='pymap3d',
 	  url='https://github.com/scienceopen/pymap3d',
           packages=['pymap3d'],
 	  )
-	  	  
+#%%
+try:
+    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+except Exception as e:
+    print('you will need to install packages in requirements.txt  {}'.format(e))
