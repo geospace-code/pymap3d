b92a23325abeffe5e1d81f2af8fc818a42a97877
michael <scienceopen@users.noreply.github.com>
2015-11-22 23:11:04 -0500

    init, untested

--- matlab/ecef2aer.m
+++ matlab/ecef2aer.m
@@ -4,6 +4,17 @@
 % Copyright (C) 2013 Sandeep V. Manthi
 
 function [az, elev, slantRange] = ecef2aer (x, y, z, lat0, lon0, h0, ell, angleut)
+  if nargin < 7 || isempty (ell)
+    ell = get_ellipsoid('wgs84');
+    angleut='degree';
+  elseif ~isstruct (ell)
+    ell = get_ellipsoid (ell);
+  end
+  
+  if nargin<8
+    angleut='degree';
+  end
+
   [xEast, yNorth, zUp] = ecef2enu (x, y, z, lat0, lon0, h0, ell, angleut);
   [az,elev,slantRange] = enu2aer (xEast, yNorth, zUp, angleut);
- end
+end
