566b980dbe13a42d6d3ca8aebf7ceb9d43fd0297
scienceopen <scienceopen@users.noreply.github.com>
2016-06-06 20:26:10 -0400

    py27

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from setuptools import setup
 import subprocess
 
 try:
-    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.cal(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
     print('you will need to install packages in requirements.txt  {}'.format(e))
 
@@ -13,12 +13,10 @@ with open('README.rst','r') as f:
 	long_description = f.read()
 
 setup(name='pymap3d',
-      version='0.1',
 	  description='3-D coordinate conversion utilities',
 	  long_description=long_description,
 	  author='Michael Hirsch',
 	  install_requires=['geopy'],
 	  url='https://github.com/scienceopen/pymap3d',
-          packages=['pymap3d'],
 	  )
 
