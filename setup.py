54cc50531e698b0cfd0314c0a7feefa1e027aeb4
scivision <scivision@users.noreply.github.com>
2016-07-11 19:55:08 -0400

    simply setup and selftest

--- setup.py
+++ setup.py
@@ -3,18 +3,13 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
-
-
-with open('README.rst','r') as f:
-	long_description = f.read()
+    pass
 
 setup(name='pymap3d',
       packages=['pymap3d'],
       description='3-D coordinate conversion utilities',
-      long_description=long_description,
       author='Michael Hirsch',
       install_requires=['pathlib2','geopy'],
       url='https://github.com/scienceopen/pymap3d',
