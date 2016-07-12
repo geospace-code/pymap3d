93e25c1655502ee554b5e306b7afae1b4c910207
scienceopen <scienceopen@users.noreply.github.com>
2016-07-11 20:02:28 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='pymap3d',
       packages=['pymap3d'],
       description='3-D coordinate conversion utilities',
       author='Michael Hirsch',
-      install_requires=['pathlib2','geopy'],
+      install_requires=['geopy'],
       url='https://github.com/scienceopen/pymap3d',
 	  )
 
