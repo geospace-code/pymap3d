45cb8db2343aca51f8b5b42729d81f1ec2fba3c8
scivision <scivision@users.noreply.github.com>
2017-02-05 03:40:27 -0500

    fallback

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+try:
+    import conda.cli
+    conda.cli.main('install','--file','requirements.txt')
+except Exception as e:
+    print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
+
 setup(name='pymap3d',
       packages=['pymap3d'],
-      install_requires=['python-dateutil','pytz','nose','numpy','astropy',
-                'geopy'],
+      install_requires=['geopy'],
 	  )
 
