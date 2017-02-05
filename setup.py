4286416d623e99e91fe62bb512062cbdf6dd8b6e
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 03:22:48 -0500

    python

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
-
 setup(name='pymap3d',
       packages=['pymap3d'],
-      install_requires=['geopy'],
+      install_requires=['python-dateutil','pytz','nose','numpy','astropy',
+                'geopy'],
 	  )
 
