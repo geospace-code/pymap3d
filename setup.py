339a89094c146afb3e0099567605506198fa7910
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 02:45:00 -0500

    fallback

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup
+
 try:
     import conda.cli
     conda.cli.main('install','--file','requirements.txt')
 except Exception as e:
     print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
 
 setup(name='pymap3d',
       packages=['pymap3d'],
