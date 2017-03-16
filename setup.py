c4be2bd64a760782058bafc9a886a350a9c44a96
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-15 23:57:02 -0400

    x

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='pymap3d',
       version = '1.1.1',
       description='Python coordinate conversions, following convention of several popular Matlab routines.',
       author = 'Michael Hirsch, Ph.D.',
-      url = 'https://github.com/scienceopen/pymap3d',
+      url = 'https://github.com/scivision/pymap3d',
       classifiers=[
       'Intended Audience :: Science/Research',
       'Development Status :: 5 - Production/Stable',
