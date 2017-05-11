d468481674ff1c7010ee1a2eb880ee25bb8e9642
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-11 04:04:01 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import pip
 try:
     import conda.cli
     conda.cli.main('install',*req)
-except Exception as e:    
+except Exception as e: 
     pip.main(['install'] + req)
 # %%
 from setuptools import setup
 
 setup(name='pymap3d',
       packages=['pymap3d'],
-      version = '1.2.3',
+      version = '1.2.4',
       description='Python coordinate conversions, following convention of several popular Matlab routines.',
       author = 'Michael Hirsch, Ph.D.',
       url = 'https://github.com/scivision/pymap3d',
