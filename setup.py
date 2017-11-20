f6169edb09cd5a9c74d3d8e4cce25204f9d95f6a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-20 02:47:19 -0500

    template

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 req = ['nose','python-dateutil','pytz','numpy','astropy']
 # %%
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='pymap3d',
-      packages=['pymap3d'],
+      packages=find_packages(),
       version = '1.2.4',
       description='Python coordinate conversions, following convention of several popular Matlab routines.',
       author = 'Michael Hirsch, Ph.D.',
