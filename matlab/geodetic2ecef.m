3dbefcab046352cf20bb81f4509ac1c97e3fa268
cchuravy <chrischuravy@gmail.com>
2018-01-24 22:45:18 -0500

    Syntax fixes, added GRS-80 ellipsoid parameters

--- matlab/geodetic2ecef.m
+++ matlab/geodetic2ecef.m
@@ -12,7 +12,7 @@ function [x,y,z] = geodetic2ecef (phi, lambda, h, ell, angleut)
     ell = get_ellipsoid('wgs84');
   elseif ~isstruct (ell)
     ell = get_ellipsoid (ell);
-  endif
+  end
 
   if nargin<5
     angleut='degree';
@@ -21,7 +21,7 @@ function [x,y,z] = geodetic2ecef (phi, lambda, h, ell, angleut)
   % Radius of curvature of the prime vertical section
   N = get_radius_normal (coord_geod(:, 1), ell);
 
-  ## Some shortnames for variables used often.
+  % Some shortnames for variables used often.
   a = ell.a;  
   b = ell.b;
   lat = coord_geod(:, 1);
