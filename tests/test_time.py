4dc1dae3adbe5f02fffbfa1b75b25391a74af4a5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-09 15:14:17 -0400

    handle more types of arrays of times

--- tests/test_time.py
+++ tests/test_time.py
@@ -2,33 +2,51 @@
 import pytest
 from pymap3d.timeconv import str2dt
 from datetime import datetime
+import numpy as np
 
 t0 = datetime(2014, 4, 6, 8)
 
 
-def test_str2dt():
+def test_types():
     assert str2dt(t0) == t0  # passthrough
     assert str2dt('2014-04-06T08:00:00') == t0
     ti = [str2dt('2014-04-06T08:00:00'), str2dt('2014-04-06T08:01:02')]
     to = [t0, datetime(2014, 4, 6, 8, 1, 2)]
     assert ti == to   # even though ti is numpy array of datetime and to is list of datetime
 
+    t1 = [t0, t0]
+    assert (np.asarray(str2dt(t1)) == t0).all()
+
+
+def test_datetime64():
+    t1 = np.datetime64(t0)
+    assert str2dt(t1) == t0
+
+    t1 = np.array([np.datetime64(t0), np.datetime64(t0)])
+    assert (str2dt(t1) == t0).all()
+
 
 def test_xarray_time():
     xarray = pytest.importorskip('xarray')
 
     t = {'time': t0}
-
     ds = xarray.Dataset(t)
     assert str2dt(ds['time']) == t0
+
+    t2 = {'time': [t0, t0]}
+    ds = xarray.Dataset(t2)
+    assert (str2dt(ds['time']) == t0).all()
 
 
 def test_pandas_time():
     pandas = pytest.importorskip('pandas')
 
     t = pandas.Series(t0)
-    assert str2dt(t) == t0
+    assert (str2dt(t) == t0).all()
+
+    t = pandas.Series([t0, t0])
+    assert (str2dt(t) == t0).all()
 
 
 if __name__ == '__main__':
-    pytest.main([__file__])
+    pytest.main(['-x', __file__])
