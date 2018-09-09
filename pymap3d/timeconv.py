204df846054b1f3c56a8148d5f9bd0314fb34482
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-09 15:14:17 -0400

    handle more types of arrays of times

--- pymap3d/timeconv.py
+++ pymap3d/timeconv.py
@@ -14,8 +14,19 @@ def str2dt(time: datetime) -> np.ndarray:
         return time
     elif isinstance(time, str):
         return parse(time)
+    elif isinstance(time, np.datetime64):
+        return time.astype(datetime)
     else:  # some sort of iterable
         try:
-            return [parse(t) for t in time]
-        except TypeError:
-            return time.values.astype('datetime64[us]').astype(datetime)
+            if isinstance(time[0], datetime):
+                return time
+            elif isinstance(time[0], np.datetime64):
+                return time.astype(datetime)
+            elif isinstance(time[0], str):
+                return [parse(t) for t in time]
+        except (IndexError, TypeError):
+            pass
+
+        # last resort--assume pandas/xarray
+
+        return time.values.astype('datetime64[us]').astype(datetime)
