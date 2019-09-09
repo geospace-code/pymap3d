95fe807e86f6eafb8c57b8b0f9358f0fd036d22c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-09 17:19:33 -0400

    add back tests

---
+++ tests/test_look_spheroid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import pytest
+from pytest import approx
+import pymap3d as pm
+from math import nan
+
+lla0 = (42, -82, 200)
+az = [0., 10., 125.]
+
+
+def test_losint():
+
+    lat, lon, sr = pm.lookAtSpheroid(*lla0, az[1], tilt=0.)
+
+    assert (lat, lon, sr) == approx(lla0)
+
+    with pytest.raises(ValueError):
+        pm.lookAtSpheroid(lla0[0], lla0[1], -1, az, 0)
+
+
+def test_array():
+    np = pytest.importorskip("numpy")
+    tilt = [30., 45., 90.]
+    lat, lon, sr = pm.lookAtSpheroid(*lla0, az, tilt)
+
+    truth = np.array([[42.00103959, lla0[1], 230.9413173],
+                      [42.00177328, -81.9995808, 282.84715651],
+                      [nan, nan, nan]])
+
+    assert np.column_stack((lat, lon, sr)) == approx(truth, nan_ok=True)
