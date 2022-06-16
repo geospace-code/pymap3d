1ecb84ecf3f81980b845dbfd94dbfdaa08339359
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-16 11:19:12 -0400

    str2dt: reduce complexity

--- src/pymap3d/timeconv.py
+++ src/pymap3d/timeconv.py
@@ -1,21 +1,19 @@
 # Copyright (c) 2014-2018 Michael Hirsch, Ph.D.
 """ convert strings to datetime """
+
+from __future__ import annotations
+
 from datetime import datetime
 
 try:
-    from dateutil.parser import parse
+    import dateutil.parser
 except ImportError:
-    parse = None  # type: ignore
-try:
-    import numpy as np
-except ImportError:
-    np = None  # type: ignore
-
+    pass
 
 __all__ = ["str2dt"]
 
 
-def str2dt(time: datetime) -> datetime:
+def str2dt(time: str | datetime) -> datetime:
     """
     Converts times in string or list of strings to datetime(s)
 
@@ -33,24 +31,32 @@ def str2dt(time: datetime) -> datetime:
     if isinstance(time, datetime):
         return time
     elif isinstance(time, str):
-        if parse is None:
-            raise TypeError("expected datetime")
-        return parse(time)
-    elif np is not None and isinstance(time, np.datetime64):
-        return time.astype(datetime)
-    else:  # some sort of iterable
         try:
-            if isinstance(time[0], datetime):
-                return time
-            elif np is not None and isinstance(time[0], np.datetime64):
-                return time.astype(datetime)
-            elif isinstance(time[0], str):
-                if parse is None:
-                    raise TypeError("expected datetime")
-                return [parse(t) for t in time]
-        except (IndexError, TypeError):
-            pass
+            return dateutil.parser.parse(time)
+        except NameError:
+            raise ImportError("pip install dateutil")
 
-        # last resort--assume pandas/xarray
+    # some sort of iterable
+    try:
+        if isinstance(time[0], datetime):
+            return time
+        elif isinstance(time[0], str):
+            return [dateutil.parser.parse(t) for t in time]
+    except IndexError:
+        pass
+    except NameError:
+        raise ImportError("pip install dateutil")
 
+    # pandas/xarray
+    try:
         return time.values.astype("datetime64[us]").astype(datetime)
+    except AttributeError:
+        pass
+
+    # Numpy.datetime64
+    try:
+        return time.astype(datetime)
+    except AttributeError:
+        pass
+
+    return time
