4aff1326f18047255655703dc695ecad97426760
Leonardo Uieda <leouieda@gmail.com>
2022-06-16 17:57:27 +0100

    Geodetic to/from geocentric spherical conversion
    Add two functions to new module `pymap3d/spherical.py`:
    `geodetic2spherical` and `spherical2geodetic` to convert between
    geocentric geodetic (latitude, longitude, altitude) and geocentric
    spherical coordinates (spherical latitude, longitude, radius).
    Test against equator and pole easily deduced values as well as
    latitude=45 with values pre-computed from the implementation in Boule.
    Add the functions to the API definition in `pymap3d/__init__.py` and to
    the list of functions in the `README.md`.

---
+++ src/pymap3d/tests/test_spherical.py
@@ -0,0 +1,88 @@
+import pytest
+from pytest import approx
+
+try:
+    import numpy.array as nparray
+except ImportError:
+
+    def nparray(*args):
+        "dummy function to convert values to arrays"
+        return args
+
+import pymap3d as pm
+
+
+ELL = pm.Ellipsoid()
+A = ELL.semimajor_axis
+B = ELL.semiminor_axis
+
+llrlla = [
+    ((0, 0, A - 1), (0, 0, -1)),
+    ((0, 90, A - 1), (0, 90, -1)),
+    ((0, -90, A + 1), (0, -90, 1)),
+    ((44.807576814237606, 270, 6367490.543857), (45, 270, 1)),
+    ((90, 0, B + 1), (90, 0, 1)),
+    ((90, 15, B - 1), (90, 15, -1)),
+    ((-90, 0, B + 1), (-90, 0, 1)),
+]
+llallr = [
+    ((0, 0, -1), (0, 0, A - 1)),
+    ((0, 90, -1), (0, 90, A - 1)),
+    ((0, -90, 1), (0, -90, A + 1)),
+    ((45, 270, 1), (44.807576814237606, 270, 6367490.543857)),
+    ((90, 0, 1), (90, 0, B + 1)),
+    ((90, 15, -1), (90, 15, B - 1)),
+    ((-90, 0, 1), (-90, 0, B + 1)),
+]
+llallr_list = [([[i] for i in lla], llr) for lla, llr in llallr]
+llrlla_list = [([[i] for i in llr], lla) for llr, lla in llrlla]
+llallr_array = [([nparray(i) for i in lla], llr) for lla, llr in llallr]
+llrlla_array = [([nparray(i) for i in llr], lla) for llr, lla in llrlla]
+
+atol_dist = 1e-6  # 1 micrometer
+
+
+@pytest.mark.parametrize("lla, llr", llallr)
+def test_geodetic2spherical(lla, llr):
+    coords = pm.geodetic2spherical(*lla)
+    assert coords[:2] == approx(llr[:2])
+    assert coords[2] == approx(llr[2], abs=atol_dist)
+
+
+@pytest.mark.parametrize("llr, lla", llrlla)
+def test_spherical2geodetic(llr, lla):
+    coords = pm.spherical2geodetic(*llr)
+    assert coords[:2] == approx(lla[:2])
+    assert coords[2] == approx(lla[2], abs=atol_dist)
+
+
+@pytest.mark.parametrize("lla, llr", llallr_list)
+def test_geodetic2spherical_list(lla, llr):
+    pytest.importorskip("numpy")
+    coords = pm.geodetic2spherical(*lla)
+    assert coords[:2] == approx(llr[:2])
+    assert coords[2] == approx(llr[2], abs=atol_dist)
+
+
+@pytest.mark.parametrize("llr, lla", llrlla_list)
+def test_spherical2geodetic_list(llr, lla):
+    pytest.importorskip("numpy")
+    coords = pm.spherical2geodetic(*llr)
+    assert coords[:2] == approx(lla[:2])
+    assert coords[2] == approx(lla[2], abs=atol_dist)
+
+
+@pytest.mark.parametrize("lla, llr", llallr_array)
+def test_geodetic2spherical_array(lla, llr):
+    pytest.importorskip("numpy")
+    coords = pm.geodetic2spherical(*lla)
+    assert coords[:2] == approx(llr[:2])
+    assert coords[2] == approx(llr[2], abs=atol_dist)
+
+
+@pytest.mark.parametrize("llr, lla", llrlla_array)
+def test_spherical2geodetic_array(llr, lla):
+    pytest.importorskip("numpy")
+    coords = pm.spherical2geodetic(*llr)
+    assert coords[:2] == approx(lla[:2])
+    assert coords[2] == approx(lla[2], abs=atol_dist)
