e8f9e11ea06768b1d8b04f5b9a5757526d018b87
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2022-10-28 16:11:28 +0200

    Continue transition to mypy strict mode (vincenty)

--- src/pymap3d/tests/test_vincenty.py
+++ src/pymap3d/tests/test_vincenty.py
@@ -2,7 +2,7 @@ import pymap3d.vincenty as vincenty
 from pytest import approx
 
 
-def test_track2():
+def test_track2() -> None:
     lats, lons = vincenty.track2(40, 80, 65, -148, npts=3)
     assert lats == approx([40, 69.633139886, 65])
     assert lons == approx([80, 113.06849104, -148])
