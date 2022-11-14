e110a04210430221b3adeb82eb48d94352118c0c
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-14 18:51:54 -0500

    test_eci: compact tolerance expressions

--- src/pymap3d/tests/test_eci.py
+++ src/pymap3d/tests/test_eci.py
@@ -4,6 +4,11 @@ import pymap3d as pm
 import pytest
 from pytest import approx
 
+try:
+    import astropy
+except ImportError:
+    astropy = None
+
 
 def test_eci2ecef():
     pytest.importorskip("numpy")
@@ -12,12 +17,7 @@ def test_eci2ecef():
     utc = datetime(2019, 1, 4, 12)
     ecef = pm.eci2ecef(*eci, utc)
 
-    try:
-        import astropy
-
-        rel = 0.0001
-    except ImportError:
-        rel = 0.025
+    rel = 0.025 if astropy is None else 0.0001
 
     assert ecef == approx([-5.7627e6, -1.6827e6, 3.1560e6], rel=rel)
 
@@ -29,12 +29,7 @@ def test_ecef2eci():
     utc = datetime(2019, 1, 4, 12)
     eci = pm.ecef2eci(*ecef, utc)
 
-    try:
-        import astropy
-
-        rel = 0.0001
-    except ImportError:
-        rel = 0.01
+    rel = 0.01 if astropy is None else 0.0001
 
     assert eci == approx([-2981810.6, 5207039.5, 3161595.1], rel=rel)
 
@@ -46,12 +41,7 @@ def test_eci2geodetic():
     utc = datetime(2019, 1, 4, 12)
     lla = pm.eci2geodetic(*eci, utc)
 
-    try:
-        import astropy
-
-        rel = 0.0001
-    except ImportError:
-        rel = 0.01
+    rel = 0.01 if astropy is None else 0.0001
 
     assert lla == approx([27.880801, -163.722058, 408850.646], rel=rel)
 
@@ -63,12 +53,7 @@ def test_geodetic2eci():
     utc = datetime(2019, 1, 4, 12)
     eci = pm.geodetic2eci(*lla, utc)
 
-    try:
-        import astropy
-
-        rel = 0.0001
-    except ImportError:
-        rel = 0.01
+    rel = 0.01 if astropy is None else 0.0001
 
     assert eci == approx([-2981784, 5207055, 3161595], rel=rel)
 
@@ -83,12 +68,7 @@ def test_eci2aer():
 
     aer = pm.eci2aer(*eci, *lla, t)
 
-    try:
-        import astropy
-
-        rel = 0.0001
-    except ImportError:
-        rel = 0.01
+    rel = 0.01 if astropy is None else 0.0001
 
     assert aer == approx([162.55, 55.12, 384013940.9], rel=rel)
 
@@ -103,12 +83,7 @@ def test_aer2eci():
 
     eci = pm.aer2eci(*aer, *lla, t)
 
-    try:
-        import astropy
-
-        rel = 0.001
-    except ImportError:
-        rel = 0.1
+    rel = 0.1 if astropy is None else 0.001
 
     assert eci == approx([-384535788.2, -51009524.3, -32566759.8], rel=rel)
 
